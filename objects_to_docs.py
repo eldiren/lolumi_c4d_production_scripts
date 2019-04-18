6ff4b009385c8c29a56e3976fb2b71a60f7824bd
Eldiren <logicielumiere@gmail.com>
2019-04-18 15:37:03 +0100

    optimized to use loutils

--- objects_to_docs.py
+++ objects_to_docs.py
@@ -45,149 +45,7 @@ class AssignMaterial(object):
         self.matname = matname
         self.objects = []
         self.groups = []
-        
-def QueryNetwork(material):
-    msg = c4d.BaseContainer()
-    msg.SetInt32(C4DTOA_MSG_TYPE, C4DTOA_MSG_QUERY_SHADER_NETWORK)
-    material.Message(c4d.MSG_BASECONTAINER, msg)
-    return msg
-
-def GetNextObject(op):
-    if op==None:
-        return None
-  
-    if op.GetDown():
-        return op.GetDown()
-  
-    while not op.GetNext() and op.GetUp():
-        op = op.GetUp()
-  
-    return op.GetNext()
- 
- 
-def GetAllObjects(odoc):
-    objects = []
-    op = odoc.GetFirstObject()
-    
-    if op is None:
-        return
-  
-    while op:
-        objects.append(op)
-        op = GetNextObject(op)
- 
-    return objects
-    
-# takes an object with poly selection and breaks it up so the 
-# selection are children of the parent, also keeps child material
-# assignments, useful in ARnold mtlx workflows, and packed disk
-# workflows since we can't reach polygroups
-def polyselectionbreak(bdoc, bobj):
-    if not bobj: return
-    
-    obj = bobj # the modeling commands change the op so we need to store this
-    gMatrix = bobj.GetMg()
-    tag = obj.GetFirstTag()
-    
-    newChildren = []
-    
-    while tag:
-        if tag.GetType() == c4d.Tpolygonselection:
-            # deselect all polygons
-            polyselection = obj.GetPolygonS()
-            polyselection.DeselectAll()
             
-            # select polygons from selection tag
-            tagselection = tag.GetBaseSelect()
-            tagselection.CopyTo(polyselection)
-            
-            #split: polygonselection to a new object
-            sec = utils.SendModelingCommand(command=c4d.MCOMMAND_SPLIT, list=[obj], mode=c4d.MODELINGCOMMANDMODE_POLYGONSELECTION, doc=bdoc)
-             
-            if not sec:
-                print 'split failed for ' + tag.GetName() 
-                continue    
-                               
-            sec[0].SetName(tag.GetName())
-            sec[0][c4d.ID_BASEOBJECT_REL_POSITION] = c4d.Vector(0,0,0)
-            sec[0][c4d.ID_BASEOBJECT_REL_ROTATION] = c4d.Vector(0,0,0)
-            
-            # remove polyselections and textures from the split and find a material to keep
-            secTag = sec[0].GetTag(c4d.Tpolygonselection)
-            
-            while secTag:
-                secTag.Remove()
-                secTag = sec[0].GetTag(c4d.Tpolygonselection)
-                
-            secTag = sec[0].GetFirstTag()
-            
-            while secTag:
-                oldSecTag = None
-                if secTag.GetType() == c4d.Ttexture:
-                    if secTag[c4d.TEXTURETAG_RESTRICTION] == tag.GetName():
-                        secTag[c4d.TEXTURETAG_RESTRICTION] = ''
-                    else:
-                        oldSecTag = secTag               
-                    
-                secTag = secTag.GetNext()
-                if oldSecTag:
-                    oldSecTag.Remove()
-            
-            # loop through tags and find any texture tags that have the selection and delete
-            oldMatTag = None
-            matTag = obj.GetFirstTag()
-            while matTag:
-                if matTag.GetType() == c4d.Ttexture:
-                    if matTag[c4d.TEXTURETAG_RESTRICTION] == tag.GetName():
-                        oldMatTag = matTag
-                        
-                matTag = matTag.GetNext()
-                
-                if oldMatTag: 
-                    oldMatTag.Remove()     
-                         
-            newChildren.append(sec[0])
-            #delete the polygons from selectiontag
-            utils.SendModelingCommand(command=c4d.MCOMMAND_DELETE, list=[obj], mode=c4d.MODELINGCOMMANDMODE_POLYGONSELECTION, doc=bdoc)
-            
-            
-        tag = tag.GetNext()
-        
-    # remove selection tags
-    tag = obj.GetTag(c4d.Tpolygonselection)
-            
-    while tag:
-        tag.Remove()
-        tag = obj.GetTag(c4d.Tpolygonselection)  
-    
-    # Optimize in order to remove loose points
-    options = c4d.BaseContainer()
-    options[c4d.MDATA_OPTIMIZE_TOLERANCE] = 0.001
-    options[c4d.MDATA_OPTIMIZE_POINTS] = True
-    options[c4d.MDATA_OPTIMIZE_POLYGONS] = False
-    options[c4d.MDATA_OPTIMIZE_UNUSEDPOINTS] = True
-    utils.SendModelingCommand(c4d.MCOMMAND_OPTIMIZE, list = [obj], mode = c4d.MODELINGCOMMANDMODE_ALL, bc = options, doc = obj.GetDocument())
-    
-    if (obj.GetPolygonCount() == 0): # no more polys                           
-        if (obj.GetChildren() > 0): # there are children remove this and replace with null then readd the children
-            objNull = c4d.BaseObject(c4d.Onull)
-            objNull.SetName(obj.GetName())
-            objParent = obj.GetUp()
-            obj.Remove()
-        
-            obj = objNull
-            bdoc.InsertObject(obj, objParent)
-            obj.SetMg(gMatrix)
-        elif (obj.GetChildren() == 0): # No children, we don't want a parent remain so let's axe this
-            objParent = obj.GetUp()
-            obj.Remove()
-            obj = objParent
-        
-    for child in newChildren:
-        child.InsertUnder(obj)
-        
-    c4d.EventAdd()
-    
 def main():
     filePath = c4d.storage.SaveDialog(c4d.FILESELECTTYPE_ANYTHING, "Export path...", c4d.FILESELECT_SAVE)
     paths = filePath.rsplit('\\', 1)
@@ -214,11 +72,11 @@ def main():
         
         # next we loop though all the objects and break the polyselections
         # up if there are any
-        objs = GetAllObjects(bldgdoc)
+        objs = loutils.GetAllObjects(bldgdoc)
         
         for obj in objs:
             if obj.GetTag(c4d.Tpolygonselection):
-                polyselectionbreak(bldgdoc, obj)
+                loutils.polyselectionbreak(bldgdoc, obj)
             
         plug = c4d.plugins.FindPlugin(1028082, c4d.PLUGINTYPE_SCENESAVER)
         if plug is None:

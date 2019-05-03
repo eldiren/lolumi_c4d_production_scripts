b6534bd1fae4fcc2b7c993f2a13ee05aa1905dad
Eldiren <logicielumiere@gmail.com>
2019-05-03 23:57:59 +0100

    using loutils for the conversion

--- matconv_arn_to_c4d.py
+++ matconv_arn_to_c4d.py
@@ -1,19 +1,8 @@
-import c4d
+import c4d, loutils
 from c4d import gui
 
 def main():
-    mats = doc.GetMaterials();
+    loutils.convertMaterials(doc, loutils.MATCONV_ARNOLD, loutils.MATCONV_C4D)
     
-    for mat in mats:
-        new_mat = c4d.BaseMaterial(c4d.Mmaterial); # create standard material
-        new_mat[c4d.ID_BASELIST_NAME] = mat.GetName() +"_c4d";
-        doc.InsertMaterial(new_mat,pred=None,checknames=True);
-        
-        # Transfer references of the old material to the new material
-        mat.TransferGoal(new_mat, False);
-        
-    c4d.EventAdd();
-    
-
 if __name__=='__main__':
     main()

dffe1a3340f3385f5ea83d277ae18d457b949a60
Eldiren <logicielumiere@gmail.com>
2020-11-20 13:17:32 -0500

    Added failure checks, changed pathing
    Script was failing if Generate Tx was on in Arnold Settings, added a dialog to notify user to check, also returns form the script if user cancels in the file dialog

--- arn_usd_root_export.py
+++ arn_usd_root_export.py
@@ -10,15 +10,26 @@ from c4d import gui, utils
 ARNOLD_SCENE_EXPORT_COMMAND = 1029993
 
 def main():
+    btn = gui.MessageDialog('Script will hang if Generate Tx is on, please cancel to check or ok to continue', type=c4d.GEMB_OKCANCEL)
+    
+    if(btn == c4d.GEMB_R_CANCEL):
+        return
+    
     filePath = c4d.storage.SaveDialog(c4d.FILESELECTTYPE_ANYTHING, "Export path...", c4d.FILESELECT_SAVE)
     paths = filePath.rsplit('\\', 1)
+    
+    if(not filePath):
+        return
 
-    filePath = paths[0] + "/"
+    filePath = paths[0] + "\\"
 
     obj = doc.GetFirstObject()
     while(obj):
         doc.SetActiveObject(obj)
         fileName = filePath + obj.GetName() + ".usd"
+
+        print(obj.GetName()) #debug
+        print(fileName) #debug
 
         options = c4d.BaseContainer()
         options.SetFilename(0, fileName) # file name
@@ -30,8 +41,6 @@ def main():
 
         doc.GetSettingsInstance(c4d.DOCUMENTSETTINGS_DOCUMENT).SetContainer(ARNOLD_SCENE_EXPORT_COMMAND, options)
         c4d.CallCommand(ARNOLD_SCENE_EXPORT_COMMAND) # export the file
-
-        print(fileName) #debug
 
         obj = obj.GetNext()
 
